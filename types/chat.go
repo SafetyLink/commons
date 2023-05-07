cf10b0d91d6b06f71722d4634bdf3f087aff3ce0
asavor <74147411+asavor@users.noreply.github.com>
2023-05-07 02:24:29 +0100

    updated table name

---
+++ types/chat.go
@@ -0,0 +1,12 @@
+package types
+
+import "time"
+
+type Chat struct {
+	ChatID         int64     `json:"chat_id"`
+	UnreadMessages int64     `json:"unread_messages"`
+	LastMessageAt  time.Time `json:"last_message_at"`
+	Viewed         bool      `json:"viewed"`
+	ViewedAt       time.Time `json:"viewed_at"`
+	Users          User      `json:"users"`
+}
--- types/user.go
+++ types/user.go
@@ -12,4 +12,11 @@ type User struct {
 	AvatarID  int64     `json:"avatar_id"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+	Chat      []Chat    `json:"chat,omitempty"`
+	Security  Security  `json:"security,omitempty"`
+}
+type Security struct {
+	Password  string    `json:"password"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeviceID  int64     `json:"device_id"`
 }
